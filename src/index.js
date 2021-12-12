3f4b3e23deed6ebe2997d4058c01aba1f7445495
nskij <n.skij0123@gmail.com>
2021-12-12 11:46:56 +0900

    update: add apollo client

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  // GraphQLサーバー指定
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  // Apolloクライアントがクエリ結果のフェッチ後にキャッシュする
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
